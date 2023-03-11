49d002abdc248c81c205788dae76ae87f4dd555a
Lukasz <stocki.lukasz@gmail.com>
2023-03-11 10:58:44 +0100

    Adds rest of HTTP methods.

--- Sources/SweetURL/URLRequestExtensions.swift
+++ Sources/SweetURL/URLRequestExtensions.swift
@@ -47,9 +47,34 @@ public extension URLRequest {
 
 public extension URLRequest {
 
+    /// Defines HTTP methods
     enum HTTPMethod: String {
+        /// Requests a representation of the specified resource.
+        /// It is used to retrieve information from the server.
         case get = "GET"
+        /// Submits an entity to the specified resource,
+        /// often causing a change in state or side effects on the server.
         case post = "POST"
+        /// Replaces the target resource with the updated content
+        /// sent in the request payload.
+        case put = "PUT"
+        /// Removes all current representations of the target
+        /// resource given by a URI.
+        case delete = "DELETE"
+        /// Applies partial modifications to a resource.
+        case patch = "PATCH"
+        /// Requests a response identical to a GET request,
+        /// but without the response body.
+        case head = "HEAD"
+        /// Describes the communication options for
+        /// the target resource.
+        case options = "OPTIONS"
+        /// Performs a message loop-back test along the path
+        /// to the target resource.
+        case trace = "TRACE"
+        ///  Establishes a network connection to a server using
+        ///  a specified tunnelling protocol.
+        case connect = "CONNECT"
     }
 
     @discardableResult
